ccaee65586af8d9b6411aa9325c887bcc77f7447
bernard-ng <ngandubernard@gmail.com>
2020-03-16 16:25:48 +0200

    feat: Add Routing and views template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CountriesComponent } from './pages/countries/countries.component';
+import { CountryComponent } from './pages/countries/country/country.component';
+import { ImageComponent } from './pages/image/image.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'countries', component: CountriesComponent },
+  { path: 'countries/:id', component: CountryComponent },
+  { path: 'image', component: ImageComponent },
+  { path: '*', component: DashboardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
