a3c64f886a3b8674a4be28a5aedead94403a04e1
bernard-ng <ngandubernard@gmail.com>
2020-03-23 11:56:54 +0200

    feat: Add Travis CI

--- src/app/pages/alerts/alerts-routing.module.ts
+++ src/app/pages/alerts/alerts-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {AlertsComponent} from "./alerts.component";
+import {AlertsComponent} from './alerts.component';
 
 const routes: Routes = [{
   path: '',
--- src/app/pages/infos/infos-routing.module.ts
+++ src/app/pages/infos/infos-routing.module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {InfosComponent} from "./infos.component";
+import {InfosComponent} from './infos.component';
 
 const routes: Routes = [{
   path: '',
